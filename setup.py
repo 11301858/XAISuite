129c51bccecbbae031dbfeca2dce02b53131b121
11301858 <66180831+11301858@users.noreply.github.com>
2022-10-22 17:42:50 -0700

    Create setup.py
    Code for module setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='XAISuite',
+      version='1.0',
+      description='Utilities for Training and Explanation Generation for Machine Learning Models'
+      author = "Shreyan Mitra"
+      py_modules=['code.XAI_Chooser'],
+      )
