e3e8ebcf3d9e7f3b9cc40c8cea5cd9181cf53707
11301858 <66180831+11301858@users.noreply.github.com>
2022-10-22 18:27:47 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
-from distutils.core import setup
+from setuptools import setup, find_namespace_packages
 
 setup(name='XAISuite',
       version='1.0',
-      description='Training and Explanation Generation Utilities for Machine Learning Models',
+      description='XAISuite: Training and Explanation Generation Utilities for Machine Learning Models',
+      long_description=open("README.md", "r", encoding="utf-8").read(),
+      long_description_content_type="text/markdown",
+      keywords="XAI Explainable AI Explanation Machine Learning Models",
+      url="https://github.com/11301858/XAISuite",
       author = "Shreyan Mitra",
-      packages=['code'],
+      install_requires=[
+        "omnixai>=1.2"
+      ],
+      py_modules=['XAI_Chooser'],
       )
