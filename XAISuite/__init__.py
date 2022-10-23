5ac42f5b4dabc33e932b125124a25f46f004f2aa
11301858 <66180831+11301858@users.noreply.github.com>
2022-10-22 18:40:17 -0700

    Update __init__.py

--- XAISuite/__init__.py
+++ XAISuite/__init__.py
@@ -1,1 +1,8 @@
-from .XAI_Chooser import XAI_Chooser
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    dist = get_distribution("XAISuite")
+except DistributionNotFound:
+    __version__ = "Please install XAISuite with setup.py"
+else:
+    __version__ = dist.version
