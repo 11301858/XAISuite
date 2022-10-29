2421cb1081fff46e8f534a46e72f18174827050c
11301858 <66180831+11301858@users.noreply.github.com>
2022-10-29 16:25:23 -0700

    Update imports.py

--- xaisuite/imports.py
+++ xaisuite/imports.py
@@ -1,9 +1,10 @@
 #Needed Libraries
 
-#Data Storage, Export, and Access
+#Data Storage, Export, Access, and Visualization
 import pandas as pd
 import csv
-from pathlib import Path 
+from pathlib import Path
+import matplotlib.pyplot as plt
 
 #Machine Learning Models and Training
 from sklearn.svm import*
