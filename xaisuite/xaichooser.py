db296e26bb20c1e81b41e1031c4f997f64c45ded
11301858 <66180831+11301858@users.noreply.github.com>
2022-12-23 16:14:01 -0800

    Update xaichooser.py

--- xaisuite/xaichooser.py
+++ xaisuite/xaichooser.py
@@ -24,8 +24,8 @@ def train_and_explainModel(model:str, tabular_data:Tabular, x_ai:list, indexList
         modeler = eval(model + "(probabaility = True)")
       else:
         modeler= eval(model + "()") #Create model function from provided model name. This will not work if model is not part of sklearn library or is unsupervised.
-    except:
-      print("Provided model name is incorrect or is not part of sklearn library. Only supervised learning models in sklearn are supported. Refer to models by their associated functions. For example, if you want to use support vector regression, pass in \"SVR\"")
+    except Exception as e:
+      print("Provided model name is incorrect or is not part of sklearn library. Only supervised learning models in sklearn are supported. Refer to models by their associated functions. For example, if you want to use support vector regression, pass in \"SVR\". \n Error message: " + e)
       return None
     
 
