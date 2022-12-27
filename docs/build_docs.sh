863439f1bc4d48e11ec76b9818d344aca66c3ff7
11301858 <66180831+11301858@users.noreply.github.com>
2022-12-26 17:19:35 -0800

    Update build_docs.sh

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -58,6 +58,7 @@ for version in $(git tag --list 'v[0-9]*'); do
         git fetch
         git rm "$f"
     done
+    git add .
     git fetch
     git checkout "tags/${version}" -- "${checkout_files[@]}"
     export current_version=${version}
