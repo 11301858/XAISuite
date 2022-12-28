2c1c326df8e0bbd2ffe9fd1d2d226fd79b30c6d7
11301858 <66180831+11301858@users.noreply.github.com>
2022-12-28 14:47:03 -0800

    Update imports.py

--- xaisuite/imports.py
+++ xaisuite/imports.py
@@ -8,19 +8,6 @@ import matplotlib.pyplot as plt
 import ast
 
 #Machine Learning Models and Training
-from sklearn.svm import*
-from sklearn.ensemble import*
-from sklearn.gaussian_process import*
-from sklearn.isotonic import*
-from sklearn.kernel_ridge import*
-from sklearn.linear_model import*
-from sklearn.mixture import*
-from sklearn.multiclass import*
-from sklearn.multioutput import*
-from sklearn.naive_bayes import*
-from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
-from sklearn.neural_network import*
-from sklearn.tree import*
 from sklearn.model_selection import train_test_split
 
 acceptedModels = {"SVC": "sklearn.svm.SVC", "NuSVC": "sklearn.svm.NuSVC", "LinearSVC": "sklearn.svm.LinearSVC", "SVR": "sklearn.svm.SVR", "NuSVR": "sklearn.svm.SVR", "LinearSVR": "sklearn.svm.LinearSVR", 
@@ -45,7 +32,24 @@ acceptedModels = {"SVC": "sklearn.svm.SVC", "NuSVC": "sklearn.svm.NuSVC", "Linea
                   "LassoLars": "sklearn.linear_model.LassoLars", "LassoLarsCV": "sklearn.linear_model.LassoLarsCV",
                   "LassoLarsIC": "sklearn.linear_model.LassoLarsIC", "OrthogonalMatchingPursuit": "sklearn.linear_model.OrthogonalMatchingPursuit",
                   "OrthogonalMatchingPursuitCV": "sklearn.linear_model.OrthogonalMatchingPursuitCV", "ARDRegression": "sklearn.linear_model.ARDRegression",
-                  "BayesianRidge": "sklearn.linear_model.BayesianRidge"
+                  "BayesianRidge": "sklearn.linear_model.BayesianRidge", "MultiTaskElasticNet": "sklearn.linear_model.MultiTaskElasticNet", 
+                  "MultiTaskElasticNetCV": "sklearn.linear_model.MultiTaskElasticNetCV", "MultiTaskLasso": "sklearn.linear_model.MultiTaskLasso",
+                  "MultiTaskLassoCV": "sklearn.linear_model.MultiTaskLassoCV", "HuberRegressor": "sklearn.linear_model.HuberRegressor",
+                  "QuantileRegressor": "sklearn.linear_model.QuantileRegressor", "RANSACRegressor": "sklearn.linear_model.RANSACRegressor",
+                  "TheilSenRegressor": "sklearn.linear_model.TheilSenRegressor", "PoissonRegressor": "sklearn.linear_model.PoissonRegressor",
+                  "TweedieRegressor": "sklearn.linear_model.TweedieRegressor", "GammaRegressor": "sklearn.linear_model.GammaRegressor", 
+                  "PassiveAggressiveRegressor": "sklearn.linear_model.PassiveAggressiveRegressor", "BayesianGaussianMixture": "sklearn.mixture.BayesianGaussianMixture",
+                  "GaussianMixture": "sklearn.mixture.GaussianMixture", 
+                  "OneVsOneClassifier": "sklearn.multiclass.OneVsOneClassifier", "OneVsRestClassifier": "sklearn.multiclass.OneVsRestClassifier", 
+                  "OutputCodeClassifier": "sklearn.multiclass.OutputCodeClassifier", "ClassifierChain": "sklearn.multioutput.ClassifierChain", 
+                   "RegressorChain": "sklearn.multioutput.RegressorChain",  "MultiOutputRegressor": "sklearn.multioutput.MultiOutputRegressor",
+                   "MultiOutputClassifier": "sklearn.multioutput.MultiOutputClassifier", "BernoulliNB": "sklearn.naive_bayes.BernoulliNB", 
+                  "CategoricalNB": "sklearn.naive_bayes.CategoricalNB", "ComplementNB": "sklearn.naive_bayes.ComplementNB", 
+                  "GaussianNB": "sklearn.naive_bayes.GaussianNB", "MultinomialNB": "sklearn.naive_bayes.MultinomialNB", 
+                  "KNeighborsClassifier": "sklearn.neighbors.KNeighborsClassifier", "KNeighborsRegressor": "sklearn.neighbors.KNeighborsRegressor", 
+                  "BernoulliRBM": "sklearn.neural_network.BernoulliRBM", "MLPClassifier": "sklearn.neural_network.MLPClassifier", "MLPRegressor": "sklearn.neural_network.MLPRegressor",  
+                  "DecisionTreeClassifier": "sklearn.tree.DecisionTreeClassifier", "DecisionTreeRegressor": "sklearn.tree.DecisionTreeRegressor",
+                  "ExtraTreeClassifier": "sklearn.tree.ExtraTreeClassifier", "ExtraTreeRegressor": "sklearn.tree.ExtraTreeRegressor"
                  }
 
 from sklearn.base import is_classifier, is_regressor
