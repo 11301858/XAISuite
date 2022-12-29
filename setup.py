c61d20647d5c12d198591ebbf3f24725c5603e37
11301858 <66180831+11301858@users.noreply.github.com>
2022-12-29 10:04:31 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_namespace_packages
 
 setup(name='XAISuite',
-      version='0.6.7',
+      version='0.6.9',
       description='XAISuite: Training and Explanation Generation Utilities for Machine Learning Models',
       long_description=open("README.md", "r", encoding="utf-8").read(),
       long_description_content_type="text/markdown",
