3c7262914c8d3343cf988a42451f4765252e8eb2
11301858 <66180831+11301858@users.noreply.github.com>
2022-12-28 18:42:54 -0800

    Update xaichooser.py

--- xaisuite/xaichooser.py
+++ xaisuite/xaichooser.py
@@ -63,7 +63,7 @@ def train_and_explainModel(model:str, tabular_data:Tabular, x_ai:list, indexList
     
     try:
       assert model in acceptedModels.keys()
-      import eval(acceptedModels.get(model))
+      exec('import ' + acceptedModels.get(model))
       modeler= eval(model + "( **modelSpecificArgs )") #Create model function from provided model name. This will not work if model is not part of sklearn library or is unsupervised.
     except Exception as e:
       print("Provided model name is incorrect or is not part of sklearn library. Only supervised learning models in sklearn are supported. Refer to models by their associated functions. For example, if you want to use support vector regression, pass in \"SVR\". \n Error message: " + str(e))
