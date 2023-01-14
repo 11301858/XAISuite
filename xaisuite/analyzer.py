1257905a484d2240dd8daa9ac0ce86222d29fb2e
11301858 <66180831+11301858@users.noreply.github.com>
2023-01-14 14:21:05 -0800

    Update analyzer.py

--- xaisuite/analyzer.py
+++ xaisuite/analyzer.py
@@ -31,10 +31,10 @@ def compare_explanations(filenames:list, verbose = False, **addendumkwargs): #An
         with open('featuresvsmodel.csv', 'w', newline='') as file:
           writer = csv.writer(file)
           writer.writerow(["Model"] + df['features'][0])
-        data = pd.read_csv("featuresvsmodel.csv")
+        data = pd.read_csv("featuresvsmodel.csv", index_col = 0)
       finally:
         data.loc[len(data.index)] = [model] + corrList
-        print("List of correlations is " + str(data))
+        print("List of correlations is \n" + str(data.head()))
         print("Correlation map for different features with given model between " + filenames[0].split()[0] + " and " + filenames[1].split()[0])
         plt.matshow(data)
         plt.show()
