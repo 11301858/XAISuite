963a50749a7928cd096dcfc05fc166053dfb5746
11301858 <66180831+11301858@users.noreply.github.com>
2023-01-23 22:25:35 -0800

    Update analyzer.py

--- xaisuite/analyzer.py
+++ xaisuite/analyzer.py
@@ -126,13 +126,14 @@ def maxImportanceScoreGenerator(filenames:list, verbose = False): #Generate the 
         maxScore = []
         maxScoreFeature = []
         for i in range(len(df["scores"])):
+            if(verbose):
+               print(df["scores"][i])
+               print (df["features"][i])
             highestscore = max(df["scores"][i], key=abs)
             indexofhighestscore = df["scores"][i].index(highestscore)
             maxScore.append(highestscore)
             if(verbose):
               print("Index of highest score: " + str(indexofhighestscore))
-              print(df["scores"][i])
-              print (df["features"][i])
             maxScoreFeature.append(df["features"][i][indexofhighestscore])
         
         maxdf[explainer + " maxScore"] = maxScore
