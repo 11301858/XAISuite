09d4d6278868f5c627696aaf264dd34bcf7144bd
11301858 <66180831+11301858@users.noreply.github.com>
2023-02-28 10:24:42 -0800

    Add files via upload

---
+++ xaisuitecli/XAISuiteCLI.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -x
+source import.sh
+
+if [[ $# -ge 2 ]]
+then
+    if [[ $1 == "--check" ]] 
+    then
+       echo Checking...
+       source check.sh ${@:2}
+    elif [[ $1 == "--train" ]]
+    then
+        echo Training...
+        source train.sh ${@:2}
+    fi 
+fi
