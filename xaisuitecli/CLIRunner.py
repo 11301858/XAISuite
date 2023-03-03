946a623f0e39142630bd01f40aabed57e0ebd316
11301858 <66180831+11301858@users.noreply.github.com>
2023-03-02 17:55:33 -0800

    Delete xaisuitecli directory

--- xaisuitecli/CLIRunner.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-from xaisuite import*
-import sys
-
-model = sys.argv[1]
-data = sys.argv[2]
-target = sys.argv[3]
-
-train_and_explainModel(model, load_data_CSV(data, target))
