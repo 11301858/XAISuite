4d816eb7c176f2b0af9fe9502e55c6379c615415
11301858 <66180831+11301858@users.noreply.github.com>
2023-03-23 12:43:46 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_namespace_packages
 
 setup(name='XAISuite',
-      version='1.0.3.5', 
+      version='1.0.4', 
       description='XAISuite: Training and Explanation Generation Utilities for Machine Learning Models',
       long_description=open("README.md", "r", encoding="utf-8").read(),
       long_description_content_type="text/markdown",
@@ -11,7 +11,8 @@ setup(name='XAISuite',
       install_requires=[
         "omnixai==1.2.4",
         "dash>=2.7.1",
-        "dash_bootstrap_components>=1.3.0"
+        "dash_bootstrap_components>=1.3.0",
+        "skorch>=0.12.1" #For integration with pytorch
       ],
       include_package_data=True,
       package_data={'': ['static/*']},
