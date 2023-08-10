16db1a5b654f15735b444f18c235cf96c357d533
11301858 <66180831+11301858@users.noreply.github.com>
2023-08-09 23:08:12 -0700

    Update explainableModel.py

--- xaisuite/explainableModel.py
+++ xaisuite/explainableModel.py
@@ -2,12 +2,15 @@ class ModelTrainer:
   '''
   Class to train an explainable machine learning model.
 
-  :param object model: The string name of the model, the function returning the model, or the model object itself. The model function must have fit() and predict() functions. A score() function is optional.
+  :param Any model: The string name of the model, the function returning the model, or the model Any itself. The model function must have fit() and predict() functions. A score() function is optional.
   :param DataProcessor withData: The data that will be used to train and test the model
-  :param str task: The type of task that the model performs. By default, "Tabular". Other options are "Vision" and "NLP"
+  :param str, optional taskType: The type of task that the model performs. By default, "Tabular". Other options are "Vision" and "NLP"
+  :param str, optional task: The task that the model performs. By default, "regression". Other option is "classification"
+  :param list, dict, optional explainers: A list of explainer names or, if specific parameters need to be passed to the explainers, a dict that contains the explainer names and explainer arguments. 
+  Ex. explainers = ["lime", "shap", "mace"] or explainers = {"lime": {"kernel_width": 3}, "shap": {"nsamples": 100}, "mace": None}
   '''
 
-  def __init__(model:object, withDataLoader: DataLoader, task:str = "Tabular", explainers:Union[str, list] = None, **modelArgs):
+  def __init__(model:Any, withData: DataProcessor, taskType:str = "Tabular", task:str = "regression", explainers:Union[list, dict] = None, **modelArgs):
     '''
     Class constructor
     '''
@@ -20,7 +23,7 @@ class ModelTrainer:
     model = tempModel
 
     try:
-      model.fit(withData.X_train, withData.y_train)
+      model.fit(withData.X_train, withDataLoader.y_train)
     except Exception as e:
       print("Model could not be fit to data: \n" + str(e))
 
@@ -34,7 +37,10 @@ class ModelTrainer:
     print("Model score is " + score)
 
     #Model has been trained by this point. Now for the explanations
-    
+
+    explainer_names = explainers if isinstance(explainers, list) else explainers.keys()
+
+    explainer = eval(taskType + "Explainer(explainers = explainer_names, mode = task, data = withData.loader.wrappedData, preprocess = withData.processor, params = explainers if isinstance(explainers, dict) else None)")
     
     
 
