37fdfed7c41541ed4bea3755f013a90663624c10
11301858 <66180831+11301858@users.noreply.github.com>
2023-08-09 21:27:35 -0700

    Update explainableModel.py

--- xaisuite/explainableModel.py
+++ xaisuite/explainableModel.py
@@ -3,8 +3,10 @@ class ModelTrainer:
   Class to train an explainable machine learning model.
 
   :param object model: The string name of the model, or the model function itself. The model function must have fit() and predict() functions. A score() function is optional.
-  :param DataProcessor withDataProcessor: The data processor 
+  :param DataProcessor withData: The data that will be used to train and test the model
+  :param str task: The type of task that the model performs. By default, "Tabular". Other options are "Vision", "NLP", "Data", and "Ranking"
   '''
 
-  def __init__(model:object, withDataProcessor: DataProcessor, task:str = "Tabular", explainers:Union[str, list] = None, **modelArgs):
+  def __init__(model:object, withData: Data, task:str = "Tabular", explainers:Union[str, list] = None, **modelArgs):
+    
     
