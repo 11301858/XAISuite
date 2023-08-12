6ce722ce20b3b9fc30111220ca3a82a320cdf9dc
11301858 <66180831+11301858@users.noreply.github.com>
2023-08-12 08:36:36 -0700

    Update insightGenerator.py

--- xaisuite/insightGenerator.py
+++ xaisuite/insightGenerator.py
@@ -3,4 +3,9 @@ class InsightGenerator:
   Class to generate insights based on explanation results
   '''
   def __init__(explanations:collections.OrderedDict):
-    
+    explainers = list(explanations.keys())
+    explainers.remove('predict')
+    for explainer in explainers:
+      temp = explanations.get(explainer)
+      
+
