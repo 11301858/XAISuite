84db910acbd3167df2e746ef4485b04d18eaeb0f
11301858 <66180831+11301858@users.noreply.github.com>
2023-08-14 01:41:25 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath(".."))
 # -- Project information -----------------------------------------------------
 
 project = "XAISuite"
-copyright = "2022, Shreyan Mitra"
+copyright = "2023, Shreyan Mitra"
 author = "Shreyan Mitra"
 
 
